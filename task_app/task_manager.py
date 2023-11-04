9dd551265341c2dc1fe9c4b834f134106b671f9f
Daniel <danizmd8@gmail.com>
2023-11-04 15:09:28 +0300

    [REFACTOR] Changed repeating logic into a decorator in TaskManager class (update tasks cache after every mutable interactions with storage)

--- task_app/task_manager.py
+++ task_app/task_manager.py
@@ -1,5 +1,18 @@
+from functools import wraps
+from typing import Callable
 from task_app.task import Task
 from task_app.storage import TaskStorage
+
+
+def _task_update_required(method: Callable):
+    @wraps(method)
+    def wrapped_method(self, *args, **kwargs):
+        result = method(self, *args, **kwargs)
+        self._tasks = self.storage.get_all()
+
+        return result
+
+    return wrapped_method
 
 
 class TaskManager:
@@ -7,13 +20,14 @@ class TaskManager:
         self.storage = storage
         self._tasks = storage.get_all()
 
-    def add_task(self, task: Task) -> None:
-        self.storage.save_task(task)
-        self._tasks = self.storage.get_all()
-
     def get_tasks(self) -> list[Task]:
         return self._tasks
 
+    @_task_update_required
+    def add_task(self, task: Task) -> None:
+        self.storage.save_task(task)
+
+    @_task_update_required
     def change_state(self, task: Task) -> None:
         if not task.completed:
             task.mark_as_completed()
@@ -21,11 +35,10 @@ class TaskManager:
             task.mark_as_uncompleted()
 
         self.storage.update_task(task)
-        self._tasks = self.storage.get_all()
 
+    @_task_update_required
     def delete_task(self, task: Task) -> None:
         self.storage.delete_task(task)
-        self._tasks = self.storage.get_all()
 
     def close(self) -> None:
         self.storage.close()
